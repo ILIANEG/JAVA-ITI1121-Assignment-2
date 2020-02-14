5f9efbaf441245bdb76b93c9fd48caefe97df14b
ILLIA NEGOVORA <45668987+ILIANEG@users.noreply.github.com>
2020-02-14 15:17:08 -0500

    Update ComputerRandomPlayer.java

--- Q1/ComputerRandomPlayer.java
+++ Q1/ComputerRandomPlayer.java
@@ -1,10 +1,14 @@
+import java.util.Random;
+
 public class HumanPlayer implements Player
 {
   public void play(TicTacToeGame game)
   {
     if(game.gameState == GameState.PLAYING)
     {
-      
+      Random randomIndex = new Random();
+      int toPlay = randomIndex.nextInt(game.lines * game.columns);
+      game.play(toPlay)
     }
     else
     {
