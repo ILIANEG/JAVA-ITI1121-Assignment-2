c728d2187fe961b2fa5240862f614fda11c8ed4a
ILLIA NEGOVORA <45668987+ILIANEG@users.noreply.github.com>
2020-02-14 13:57:22 -0500

    Update HumanPlayer.java

--- Q1/HumanPlayer.java
+++ Q1/HumanPlayer.java
@@ -1,4 +1,28 @@
+import java.util.Scanner;
+
 public class HumanPlayer implements Player
 {
-
+  public void play(TicTacToeGame game)
+  {
+    if(game.gameState == GameState.PLAYING)
+    {
+      if(game.getLevel() % 2 == 0)
+      {
+        String message = "X to play: "
+      }
+      else
+      {
+        String message = "O to play: "
+      }
+      Scanner input = new Scanner(System.in);
+      System.out.print(message);
+      int toPlay = reader.nextInt();
+      reader.close();
+      game.play(toPlay);
+    }
+    else
+    {
+      System.out.println("The game can not be played")
+    }
+  }
 }
