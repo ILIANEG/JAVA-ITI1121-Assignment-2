26aedfad69c1d6fa4cc8261be31b235fd851c4ba
ILLIA NEGOVORA <45668987+ILIANEG@users.noreply.github.com>
2020-02-14 14:01:43 -0500

    Update ComputerRandomPlayer.java

--- Q1/ComputerRandomPlayer.java
+++ Q1/ComputerRandomPlayer.java
@@ -1,1 +1,14 @@
-
+public class HumanPlayer implements Player
+{
+  public void play(TicTacToeGame game)
+  {
+    if(game.gameState == GameState.PLAYING)
+    {
+      
+    }
+    else
+    {
+      System.out.println("ERROR: The game can not be played")
+    }
+  }
+}
