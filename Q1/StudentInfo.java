ef4043c514d9b3a8fb460116786bfbdb1ad4c1ea
nickbailuc <35620734+nickbailuc@users.noreply.github.com>
2020-02-15 22:45:26 -0500

    Update StudentInfo.java

--- Q1/StudentInfo.java
+++ Q1/StudentInfo.java
@@ -16,9 +16,9 @@ public class StudentInfo {
     public static void display() {
 
         System.out.println("************************************************************");
-        System.out.println("*                                                          *");
-        System.out.println("*                                                          *");
-        System.out.println("*                                                          *");
+        System.out.println("*    Student 1: Nick Bailuc, 300014335, Section C-02       *");
+        System.out.println("*    Student 2: Illia Negovora, 300070880, Section C-01    *");
+        System.out.println("*    Course code: ITI1121                                  *");
         System.out.println("*                                                          *");
         System.out.println("************************************************************");
         System.out.println();
