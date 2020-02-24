3fca1d91704f9ee4f2f095f37420c908d1a79340
ILLIA NEGOVORA <45668987+ILIANEG@users.noreply.github.com>
2020-02-23 21:22:09 -0500

    Update TicTacToe.java

--- Q3/TicTacToe.java
+++ Q3/TicTacToe.java
@@ -11,38 +11,7 @@ public class TicTacToe{
         int columns = 3;
         int win = 3;
 
-   
-        try{
-            if (args.length >= 2) {
-                lines = Integer.parseInt(args[0]);
-                if(lines<2){
-                    System.out.println("Invalid argument, using default...");
-                    lines = 3;
-                }
-                columns = Integer.parseInt(args[1]);
-                if(columns<2){
-                    System.out.println("Invalid argument, using default...");
-                    columns = 3;
-                }
-            }
-            if (args.length >= 3){
-                win = Integer.parseInt(args[2]);
-                if(win<2){
-                    System.out.println("Invalid argument, using default...");
-                    win = 3;
-                }
-            } 
-            if (args.length > 3){
-                System.out.println("Too many arguments. Only the first 3 are used.");
-            } 
-
-        } catch(NumberFormatException e){
-            System.out.println("Invalid argument, using default...");
-            lines   = 3;
-            columns  = 3;
-            win = 3;
-        }		
-
+	//Only 3x3 game will be generated	
         LinkedList<LinkedList<TicTacToeGame>> allGames;
 		allGames = ListOfGamesGenerator.generateAllGames(lines,columns,win);
 		int total = 0;
