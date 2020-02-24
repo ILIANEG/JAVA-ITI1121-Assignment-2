86ecd804ff9e7737d1bb685c3a1bafd58f22ec95
ILLIA NEGOVORA <45668987+ILIANEG@users.noreply.github.com>
2020-02-23 20:12:19 -0500

    Update HumanPlayer.java

--- Q1/HumanPlayer.java
+++ Q1/HumanPlayer.java
@@ -2,21 +2,35 @@ import java.io.Console;
 
 public class HumanPlayer implements Player
 {
-	/*	The purpose of this class is to parse HumanPlayer input, handle round
-		rotation, and display their respective messages.*/
+	//implements play for player, which will display the board and expect valid input
 	public void play(TicTacToeGame game)
 	{
 		if (game.getGameState() == GameState.PLAYING)
 		{
-			System.out.println("Human Player's turn");
+			System.out.println("Player 1's turn");
 			System.out.println(game.toString());
+			//checks if X or O plays and prints respective message
 			if (game.getLevel() % 2 == 0) System.out.print("X to play: ");
 			else System.out.print("O to play: ");
-			Console console = System.console();
-			console = System.console();
-			String StringToPlay = console.readLine();
-			int toPlay = Integer.parseInt(StringToPlay);
-			game.play(toPlay - 1);
+			boolean played = false;
+			//will ask player for input untill the valid one is provided
+			while(! played)
+			{
+				Console console = System.console();
+				console = System.console();
+				String StringToPlay = console.readLine();
+				int toPlay = Integer.parseInt(StringToPlay);
+				if(game.valueAt(toPlay - 1) == CellValue.EMPTY && 0 < toPlay && toPlay < gamle.lines * game.columns )
+				{
+					game.play(toPlay - 1);
+					played = true;
+				}
+				else
+				{
+					System.out.println("Invalid input, please try to play another cell");
+				}
+			}
+			
 		}
 		else System.out.println("ERROR: The game can not be played");
 	}
