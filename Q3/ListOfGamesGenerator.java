034c27bd37a24eaa04efa2e807de92df1d0c12af
ILLIA NEGOVORA <45668987+ILIANEG@users.noreply.github.com>
2020-02-23 21:14:24 -0500

    Update ListOfGamesGenerator.java

--- Q3/ListOfGamesGenerator.java
+++ Q3/ListOfGamesGenerator.java
@@ -5,12 +5,17 @@ public class ListOfGamesGenerator {
 	//Function that will generate all possible games taking symmetry into consideration
 	public static LinkedList<LinkedList<TicTacToeGame>> generateAllGames(int lines, int columns, int winLength)
 	{
+		//Creates an empty list of lists
 		LinkedList<LinkedList<TicTacToeGame>> allGames = new LinkedList<LinkedList<TicTacToeGame>>();
+		//Create new "empty" game
 		TicTacToeGame game = new TicTacToeGame(lines, columns, winLength);
+		//Add "empty" game to the allgame list
 		allGames.add(new LinkedList<TicTacToeGame>());
 		allGames.get(0).add(game);
+		
 		boolean allBranchesClosed = false;
 		int branchIndex = 0;
+		//loop generates new "branches" or levels until no more populated levels can be created
 		while(!allBranchesClosed)
 		{
 			LinkedList<TicTacToeGame> branch = nextLevelBranch(allGames.get(branchIndex));
@@ -26,18 +31,23 @@ public class ListOfGamesGenerator {
 		}
 		return allGames;
 	}
-
+	
+	//method generates "branch" or level, based on previous level, in this version symmetry will be taken in consideration
 	private static LinkedList<TicTacToeGame> nextLevelBranch(LinkedList<TicTacToeGame> g)
 	{
+		//Create new empty branch
 		LinkedList<TicTacToeGame> branch = new LinkedList<TicTacToeGame>();
+		//Iterate over every game of previous branch
 		for(int i = 0; i < g.size(); i++)
 		{
 			TicTacToeGame curGame = g.get(i);
+			//Iterate over every cell of the game
 			for(int c = 0; c < curGame.lines * curGame.columns && curGame.getGameState() == GameState.PLAYING; c++)
 			{
 				if(curGame.valueAt(c) == CellValue.EMPTY)
 				{
 					TicTacToeGame gameToAdd = new TicTacToeGame(curGame, c);
+					//Checks if game to be added is unique and no symmetrical identities found
 					if(! branch.contains(gameToAdd)
 						&& ! branch.contains(inverseBoard(gameToAdd))
 						&& ! branch.contains(reverseRows(gameToAdd))
@@ -55,8 +65,8 @@ public class ListOfGamesGenerator {
 		}
 		return branch;
 	}
-///////////////////////////////////////////////////////////////////////////////////////////////////
-	//Method creates TicTaToeGame with reversed rows
+	
+	//Method generates TicTacToeGame with reversed rows based on another TicTacToeGame
 	private static TicTacToeGame reverseRows(TicTacToeGame game)
 	{
 		//Creating boradOfGame array that stores the board of passed in game
@@ -68,17 +78,18 @@ public class ListOfGamesGenerator {
 		}
 		//You need to populate this board in such way that the rows are reversed order
 		CellValue[] revRowBoard = new CellValue[game.lines * game.columns];
-		//CREATE AND IMPLEMENT ALGORYTHM THAT WILL USE boardOfGame TO POPULATE revRowBoard
+		//Algorythm that populates revRowBoard
 		for (int a = game.lines - 1; a > -1; a--) for (int b = 0; b < game.columns; b++)
 		{
 			revRowBoard[count] = boardOfGame[game.lines*a+b];
 			count++;
 		}
-		//If your revRowBoard is good, my new constructor (line #120 in TicTacToeGame.java) should take care about creating new game;
+		//New constructor (line #120 in TicTacToeGame.java) creates game by copying conditions of previous game and new board
+		//Such approach allows to reuse the code which checks the existance of particular game in the branch
 		return new TicTacToeGame(game, revRowBoard);
 	}
 
-	//Method creates TicTacToe with reversed columns
+	//Method generates TicTacToeGame with reversed columns based on another TicTacToeGame
 	private static TicTacToeGame reverseColumns(TicTacToeGame game)
 	{
 		//Creating boradOfGame array that stores the board of passed in game
@@ -88,9 +99,8 @@ public class ListOfGamesGenerator {
 		{
 			boardOfGame[i] = game.valueAt(i);
 		}
-		//You need to populate this board in such way that the columns are reversed order
 		CellValue[] revColBoard = new CellValue[game.lines * game.columns];
-		//this loop reverse the columns of the board
+		//This loop reverse the columns of the board
 		for (int c = 0; c < game.columns; c++) for (int d = game.lines - 1; d > -1; d--)
 		{
 			revColBoard[count] = boardOfGame[game.lines*c+d];
@@ -98,6 +108,7 @@ public class ListOfGamesGenerator {
 		}
 		return new TicTacToeGame(game, revColBoard);
 	}
+	//Creates a TicTacToeGame with columns = rows, rows = columns based on another TicTacToeGame
 	private static TicTacToeGame inverseBoard(TicTacToeGame game)
 	{
 		CellValue[] boardOfGame = new CellValue[game.lines * game.columns];
@@ -107,6 +118,7 @@ public class ListOfGamesGenerator {
 			boardOfGame[i] = game.valueAt(i);
 		}
 		CellValue[] inverseBoard = new CellValue[game.lines * game.columns];
+		//turn columns into rows and rows into columns
 		for(int c = 0; c < game.columns; c++) for(int l = 0; l < game.lines; l++)
 		{
 			inverseBoard[count] = boardOfGame[c + l * game.columns];
